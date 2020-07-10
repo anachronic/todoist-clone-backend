1bd09737f5592dde2f9c9c92ed37af8a4e89d55b
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-09 22:19:23 -0400

    feat: allow querying for inbox project

--- src/resolvers/ProjectResolver.ts
+++ src/resolvers/ProjectResolver.ts
@@ -1,27 +1,69 @@
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware, Args } from 'type-graphql'
 import { ServerContext } from '../config/apollo'
 import { Project } from '../entities/Project'
 import { User } from '../entities/User'
 import { NotAuthenticated } from '../errors/NotAuthenticated'
 import { ProjectCreateInput } from './types/ProjectCreateInput'
 import { needsAuth } from '../middleware/auth'
+import { ProjectInput } from './types/ProjectInput'
 
 @Resolver()
 export class ProjectResolver {
   @Query(() => [Project])
   @UseMiddleware(needsAuth)
-  async projects(@Ctx() { user }: ServerContext): Promise<Project[]> {
+  async projects(
+    @Ctx() { user }: ServerContext,
+    @Args() { includeInbox = false }: ProjectInput
+  ): Promise<Project[]> {
     const userId = user?.id
 
     if (!userId) {
       throw new NotAuthenticated('This query needs authentication')
     }
 
-    const projects = await Project.createQueryBuilder('project')
-      .where('project.user = :userId', { userId })
-      .getMany()
+    const qb = Project.createQueryBuilder('project').where('project.user.id = :userId', { userId })
 
-    return projects
+    if (!includeInbox) {
+      qb.andWhere('lower(project.name) <> :name', { name: 'inbox' })
+    }
+
+    return await qb.getMany()
+  }
+
+  @Query(() => Project)
+  @UseMiddleware(needsAuth)
+  async inbox(@Ctx() { user }: ServerContext): Promise<Project> {
+    const userId = user?.id
+
+    if (!userId) {
+      throw new NotAuthenticated()
+    }
+
+    const qb = Project.createQueryBuilder('project')
+      .where('lower(project.name) = :name', {
+        name: 'inbox',
+      })
+      .andWhere('project.user.id = :userId', { userId })
+
+    let inbox = await qb.getOne()
+
+    if (inbox) {
+      return inbox
+    }
+
+    const currentUser = await User.findOne({ id: userId })
+
+    if (!currentUser) {
+      throw new NotAuthenticated()
+    }
+
+    inbox = Project.create({
+      name: 'Inbox',
+    })
+
+    inbox.user = currentUser
+    await inbox.save()
+    return inbox
   }
 
   @Mutation(() => Project)
