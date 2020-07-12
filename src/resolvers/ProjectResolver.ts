639cb5d3be7db1c1edca68434a7ca936d01aa5a9
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-12 01:52:05 -0400

    feat: allow updating project config
    This allows changing name or color for projects

--- src/resolvers/ProjectResolver.ts
+++ src/resolvers/ProjectResolver.ts
@@ -1,14 +1,41 @@
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware, Args } from 'type-graphql'
+import {
+  Arg,
+  Args,
+  Ctx,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+  FieldResolver,
+  Root,
+} from 'type-graphql'
 import { ServerContext } from '../config/apollo'
 import { Project } from '../entities/Project'
 import { User } from '../entities/User'
 import { NotAuthenticated } from '../errors/NotAuthenticated'
-import { ProjectCreateInput } from './types/ProjectCreateInput'
 import { needsAuth } from '../middleware/auth'
+import { ProjectCreateInput } from './types/ProjectCreateInput'
 import { ProjectInput } from './types/ProjectInput'
+import { ProjectUpdateInput } from './types/ProjectUpdateInput'
+import { ProjectColor } from '../entities/ProjectColor'
+import { Task } from '../entities/Task'
 
-@Resolver()
+@Resolver(Project)
 export class ProjectResolver {
+  @FieldResolver()
+  async tasks(
+    @Root() project: Project,
+    @Arg('done', { nullable: true }) done?: boolean
+  ): Promise<Task[]> {
+    const tasks = await project.tasks
+
+    if (typeof done !== 'undefined') {
+      return tasks.filter((task: Task) => task.done === done)
+    }
+
+    return tasks
+  }
+
   @Query(() => [Project])
   @UseMiddleware(needsAuth)
   async projects(
@@ -66,6 +93,27 @@ export class ProjectResolver {
     return inbox
   }
 
+  @Query(() => Project)
+  @UseMiddleware(needsAuth)
+  async project(@Ctx() { user }: ServerContext, @Arg('id') id: string): Promise<Project> {
+    const userId = user?.id
+
+    if (!userId) {
+      throw new NotAuthenticated()
+    }
+
+    const qb = Project.createQueryBuilder('project')
+      .where('project.id = :id', { id: +id })
+      .andWhere('project.user.id = :userId', { userId })
+
+    const result = await qb.getOne()
+    if (!result) {
+      throw new Error('No project found')
+    }
+
+    return result
+  }
+
   @Mutation(() => Project)
   @UseMiddleware(needsAuth)
   async createProject(
@@ -82,6 +130,44 @@ export class ProjectResolver {
     const project = Project.create(projectInput)
     project.user = user
 
+    await project.save()
+
+    return project
+  }
+
+  @Mutation(() => Project)
+  @UseMiddleware(needsAuth)
+  async updateProject(
+    @Arg('project') { id, name, colorId }: ProjectUpdateInput,
+    @Ctx() { user: ctxUser }: ServerContext
+  ): Promise<Project> {
+    const userId = ctxUser?.id
+
+    if (!userId) {
+      throw new NotAuthenticated('This mutation needs authentication')
+    }
+
+    const qb = Project.createQueryBuilder('project')
+      .where('project.id = :id', { id })
+      .andWhere('project.user.id = :userId', { userId })
+
+    const project = await qb.getOne()
+
+    if (!project) {
+      throw new Error('No project found')
+    }
+
+    if (name) {
+      project.name = name
+    }
+
+    if (typeof colorId !== 'undefined') {
+      const color = await ProjectColor.findOne({ id: colorId })
+      if (!color) {
+        throw new Error('Color not found')
+      }
+      project.color = color
+    }
     await project.save()
 
     return project
---
+++ src/resolvers/types/ProjectUpdateInput.ts
@@ -0,0 +1,14 @@
+import { Field, InputType } from 'type-graphql'
+import { Project } from '../../entities/Project'
+
+@InputType()
+export class ProjectUpdateInput implements Omit<Partial<Project>, 'id'> {
+  @Field(() => String)
+  id: string
+
+  @Field({ nullable: true })
+  name?: string
+
+  @Field({ nullable: true })
+  colorId?: number
+}
