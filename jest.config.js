7ea1640610bddd0cb6cfd16329042a208817ba2e
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-16 04:22:50 -0400

    chore(jest): prepare coverage for CI

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!**/node_modules/**',
+    '!**/src/tests/**',
+    '!src/migrations/**',
+  ],
 }
