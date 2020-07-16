d43ea0258f7fdc6891146cffc70521dfba885e59
Nicolás Salas <nicolas.salas@imfd.cl>
2020-07-16 03:46:34 -0400

    test: create tests for User

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,5 +1,7 @@
-import { createConnection, Connection } from 'typeorm'
 import Logger from 'bunyan'
+import dotenv from 'dotenv'
+import { Connection, createConnection, getConnectionOptions } from 'typeorm'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 
 export async function setupDatabase(logger: Logger): Promise<Connection> {
   try {
@@ -9,5 +11,31 @@ export async function setupDatabase(logger: Logger): Promise<Connection> {
   } catch (err) {
     logger.error('Database is not fine')
     throw err
+  }
+}
+
+export async function setupTestingDatabase(): Promise<Connection> {
+  dotenv.config({ path: '.prod.env' })
+  dotenv.config({ path: '.development.env' })
+  dotenv.config({ path: '.default.env' })
+
+  const options = await getConnectionOptions()
+
+  const testOptions: PostgresConnectionOptions = {
+    ...options,
+    type: 'postgres',
+    database: 'todoist_clone_test',
+    dropSchema: true,
+    logging: false,
+    synchronize: true,
+    migrationsRun: false,
+  }
+
+  try {
+    return await createConnection(testOptions)
+  } catch (err) {
+    console.error('failed to setup testing database. Exiting...')
+    console.trace(err)
+    process.exit(-1)
   }
 }
